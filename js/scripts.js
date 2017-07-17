5c42671b21f738a6e34e5c9fcda01e0576573e65
tk <tk@tk.pl>
2017-07-17 16:52:45 +0200

    ok

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+﻿var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var listNumber = document.getElementsByTagName('li')
+	
+	console.log(list);
+	console.log(listNumber.length);
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + listNumber.length;
+	list.appendChild(element);
+
+});
